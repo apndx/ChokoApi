5b6a7fa95a082c891d5800410d8005142008b2b6
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-02-23 16:27:01 +0200

    Add the price calculator functionality, update readme to instruct in usage

--- README.md
+++ README.md
@@ -2,16 +2,30 @@
 
 API to get the cheapest chocolate prices by combining boxes, packs and bars.
 
-Prices
+The API receives the number of chocolate bars needed by the user, the prices of bars, packs and boxes, and the number of bars included in a pack and box.
+The API then returns the cheapest combination of boxes, packs and individual bars to satisfy the requirement, as well as the total price.
 
-| Size | Price € |
-|------|---------|
-| Bar  | 2.30    |
-| Pack | 25      |
-| Box  | 230     |
+Example usage:
+
+POST /price
+{
+  "count": 150,
+  "barPrice": 2.3,
+  "packPrice": 25,
+  "boxPrice": 230,
+  "packSize": 12,
+  "boxSize": 120
+}
+
 
 Start application in [local mode](http://localhost:3003/):
 
 ```
 npm start
+```
+
+
+Start application in [development mode](http://localhost:3003/):
+```
+npm run dev
 ```
--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const app = express()
 
-let prices = [
-  {
-    "boxCount": 1,
-    "packCount": 2,
-    "barCount": 6,
-    "totalPrice": 293.8
-  }
-]
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Welcome to browse chocolate prices!</h1>')
 })
 
-app.get('/price', (request, response) => {
-  response.json(prices)
+app.post('/price', (request, response) => {
+
+  priceRequest = request.body
+
+  boxCount = Math.floor(priceRequest.count / priceRequest.boxSize)
+  packCount = Math.floor((priceRequest.count % priceRequest.boxSize) / priceRequest.packSize)
+  barCount = (priceRequest.count % priceRequest.boxSize) % priceRequest.packSize
+
+  totalPrice = boxCount * priceRequest.boxPrice + packCount * priceRequest.packPrice + barCount * priceRequest.barPrice
+
+  priceResponse = {
+    'boxCount' : boxCount,
+    'packCount': packCount,
+    'barCount': barCount,
+    'totalPrice': totalPrice
+  }
+
+  response.json(priceResponse)
 })
 
 const PORT = 3003
